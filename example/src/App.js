3176701d7e63b8ac8e8b4eda34c0f9d99ebae3b4
Binod Kumar Swain <binodkumarswain94@gmail.com>
2019-09-21 01:51:13 +0530

    fix(): changes for repo github page

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,55 @@
 import React, { Component } from 'react';
 import Faq from 'react-faq-component';
+import Prism from "prismjs";
+
+const htmlcode = `import React, { Component } from 'react';
+import Faq from 'react-faq-component';
+
+const data = {
+  title: "FAQ (How it works)",
+  rows: [
+    {
+      title: "Lorem ipsum dolor sit amet,",
+      content: "Lorem ipsum dolor sit amet, consectetur "
+    },
+    {
+      title: "Nunc maximus, magna at ultricies elementum",
+      content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
+    },
+    {
+      title: "Curabitur laoreet, mauris vel blandit fringilla",
+      content: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+    },
+    {
+      title: "What is the package version",
+      content: "v1.0.0"
+    }]
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Faq data={data}/>
+      </div>
+    )
+  }
+}
+`;
+
+const dataJSON = {
+  title: "FAQ title",
+  rows: [
+    {
+      title: "Question 1",
+      content: "Answer 1"
+    },
+    {
+      title: "Question 2",
+      content: "Answer 2"
+    }
+  ]
+}
 
 const data = {
   title: "FAQ (How it works)",
@@ -32,7 +82,60 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Faq data={data}/>
+        <h1>react-faq-component demo</h1>
+        <section className="badges">
+          <a href="https://github.com/binodswain/react-faq-component">
+            <img src="https://badgen.net/github/release/binodswain/react-faq-component" alt="release"/>
+          </a>
+          <a href="https://github.com/binodswain/react-faq-component/issues">
+            <img src="https://badgen.net/github/issues/binodswain/react-faq-component" alt="issues"/>
+          </a>
+          <a href="https://github.com/binodswain/react-faq-component/blob/master/LICENSE">
+            <img src="https://badgen.net/github/license/binodswain/react-faq-component" alt="license"/>
+          </a>
+          <a href="https://github.com/binodswain/react-faq-component/commits/">
+            <img src="https://badgen.net/github/last-commit/binodswain/react-faq-component" alt="last commit"/>
+          </a>
+        </section>
+        <div className="faq-wrapper">
+          <Faq data={data} />
+        </div>
+        <hr/>
+        <section className="usage">
+          <h3>Usage</h3>
+          <div>Install the package.</div>
+          <pre>
+            <code className="language-markup">
+              npm install --save react-faq-component
+            </code>
+          </pre>
+          <div>Import the package.</div>
+          <pre>
+            <code className="language-markup">
+              import Faq from 'react-faq-component';
+            </code>
+          </pre>
+          <br/>
+          <div>example: </div><br/>
+          <pre className="line-numbers">
+            <code className="language-js">
+              {htmlcode}
+            </code>
+          </pre>
+
+        </section>
+
+        <section className="dataformat">
+          <h3>Data format</h3>
+          <p>The data passed into <strong>react-faq-component</strong> is an object having two keys i.e <strong>title</strong> (String) and <strong>rows</strong> (array of objects).
+            
+          </p>
+          <pre className="line-numbers">
+            <code className="language-json">
+              {JSON.stringify(dataJSON, null, 4)}
+            </code>
+          </pre>
+        </section>
       </div>
     )
   }
